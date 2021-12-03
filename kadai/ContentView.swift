49b84b725e9a910eadcdce16c14b9e6f42852f53
mana.u <great8life@gmail.com>
2021-12-04 01:45:05 +0900

    first

--- kadai/ContentView.swift
+++ kadai/ContentView.swift
@@ -1,16 +1,44 @@
-//
-//  ContentView.swift
-//  kadai
-//
-//  Created by 上田学 on 2021/12/03.
-//
-
 import SwiftUI
 
 struct ContentView: View {
+    @State private var num1: Int
+    @State private var num2: Int
+    @State private var num3: Int
+    @State private var num4: Int
+    @State private var num5: Int
+    @State private var ans: Int
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        HStack {
+            VStack(alignment: .leading) {
+                TextField("", value: $num1, format: .number)
+                    .frame(width: 150, height: 50)
+                    .border(.secondary)
+                    .padding(.leading)
+                TextField("", value: $num2, format: .number)
+                    .frame(width: 150, height: 50)
+                    .border(.secondary)
+                    .padding(.leading)
+                TextField("", value: $num3, format: .number)
+                    .frame(width: 150, height: 50)
+                    .border(.secondary)
+                    .padding(.leading)
+                TextField("", value: $num4, format: .number)
+                    .frame(width: 150, height: 50)
+                    .border(.secondary)
+                    .padding(.leading)
+                TextField("", value: $num5, format: .number)
+                    .frame(width: 150, height: 50)
+                    .border(.secondary)
+                    .padding(.leading)
+                Button("Button", action: {ans = num1 + num2})
+                    .padding(.leading)
+                Text("1")
+                    .padding([.top, .leading])
+                Spacer()
+            }
+            .textFieldStyle(.roundedBorder)
+            Spacer()
+        }
     }
 }
 
--- kadai/kadaiApp.swift
+++ kadai/kadaiApp.swift
@@ -1,14 +1,7 @@
-//
-//  kadaiApp.swift
-//  kadai
-//
-//  Created by 上田学 on 2021/12/03.
-//
-
 import SwiftUI
 
 @main
-struct kadaiApp: App {
+struct KadaiApp: App {
     var body: some Scene {
         WindowGroup {
             ContentView()
