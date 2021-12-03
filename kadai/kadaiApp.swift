5608e3da40747a8dded51a4bc9e683602aa4ad38
mana.u <great8life@gmail.com>
2021-12-03 23:02:50 +0900

    Initial Commit

---
+++ kadai/kadaiApp.swift
@@ -0,0 +1,17 @@
+//
+//  kadaiApp.swift
+//  kadai
+//
+//  Created by 上田学 on 2021/12/03.
+//
+
+import SwiftUI
+
+@main
+struct kadaiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
