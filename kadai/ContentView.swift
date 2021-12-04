e5666c51a5bd9d014446d5e8cef603e0fce29a70
akio0911 <akio0911@gmail.com>
2021-12-05 07:07:05 +0900

    feedback

--- kadai/ContentView.swift
+++ kadai/ContentView.swift
@@ -7,34 +7,18 @@ struct ContentView: View {
     @State private var num4: Int?
     @State private var num5: Int?
     @State private var ans: Int = 0
+
     var body: some View {
         HStack {
             VStack(alignment: .leading) {
-                TextField("", value: $num1, format: .number)
-                    .frame(width: 150, height: 50)
-                    .border(.secondary)
-                    .padding(.leading)
-                TextField("", value: $num2, format: .number)
-                    .frame(width: 150, height: 50)
-                    .border(.secondary)
-                    .padding(.leading)
-                TextField("", value: $num3, format: .number)
-                    .frame(width: 150, height: 50)
-                    .border(.secondary)
-                    .padding(.leading)
-                TextField("", value: $num4, format: .number)
-                    .frame(width: 150, height: 50)
-                    .border(.secondary)
-                    .padding(.leading)
-                TextField("", value: $num5, format: .number)
-                    .frame(width: 150, height: 50)
-                    .border(.secondary)
-                    .padding(.leading)
-                Button("Button", action: {ans = num1 ?? 0
-                    ans += num2 ?? 0
-                    ans += num3 ?? 0
-                    ans += num4 ?? 0
-                    ans += num5 ?? 0
+                NumberField(number: $num1)
+                NumberField(number: $num2)
+                NumberField(number: $num3)
+                NumberField(number: $num4)
+                NumberField(number: $num5)
+                Button("Button", action: {
+                    ans = [num1, num2, num3, num4, num5]
+                        .map { $0 ?? 0 }.reduce(0, +)
                 })
                     .padding(.leading)
                 Text(ans, format: .number)
@@ -43,6 +27,17 @@ struct ContentView: View {
             }
             Spacer()
         }
+    }
+}
+
+struct NumberField: View {
+    @Binding var number: Int?
+
+    var body: some View {
+        TextField("", value: $number, format: .number)
+            .frame(width: 150, height: 50)
+            .border(.secondary)
+            .padding(.leading)
     }
 }
 
